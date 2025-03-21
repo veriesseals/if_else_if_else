aecdee41dc4fc3d9b7b08e844729108bdc38d06d
Veries Seals <veries@me.com>
2025-03-20 19:47:37 -0500

    Created an if else if else code block

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // if / then statements
+        // -----------------------------------------
+        
+        // Variables Assigned
+        // -----------------------------------------
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+        
+        // Created an if / else if / else Statement
+        // -----------------------------------------
+        if (score < 5000 && score > 1000)  {
+            System.out.println("Your score was less than 5000 but greater than 1000");
+        } else if (score < 1000) {
+            System.out.println("Your Score was less than 1000");
+        } else {
+            System.out.println("Got here");
+        }
+        
+    }
+}
